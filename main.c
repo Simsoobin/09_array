c65d7ee3a29b696e7ecf5623381286b70cb9b808
Simsoobin <55381009+Simsoobin@users.noreply.github.com>
2019-10-31 23:48:53 +0900

    9_2

--- main.c
+++ main.c
@@ -4,17 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
+	int i2;
 	int grade[5];
+	int sum, average;
 	
-	grade[0]=10;
-	grade[1]=20;
-	grade[2]=30;
-	grade[3]=40;
-	grade[4]=50;
-	for (i=0; i<5; i++)	
-	{
-		printf("grade[%i] = %i\n", i, grade[i]);
+	for(i2=0; i2<5; i2++){
+		
+		 grade[i2]= rand() %100;
+	}
+	printf("\n");
+	
+	for(i2=0; i2<5; i2++){
+		printf("grade[%d] =%d\n", i2, grade[i2]);
 	}
 	return 0;
 }
