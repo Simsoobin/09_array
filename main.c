20c05588ce0ce9c501f63fb1f3010f4501b40f5c
Simsoobin <55381009+Simsoobin@users.noreply.github.com>
2019-11-01 17:33:01 +0900

    9_3.1

--- main.c
+++ main.c
@@ -4,14 +4,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-    int i5;
-    int a[5]={1, 2, 3, 4, 5};
-    int b[5]={1, 2, 3, 4, 5};
+    int i4;
+    int grade2[5];
+    int score[5];
     
-    for(i5=0; i5<5; i5++) {
-    	 if (a[i5] !=b[i5]){
-    	 	printf("%i is different\n", i5);
-    	      }
-		 }
+    for(i4=0; i4<5; i4++) {
+    	grade2[i4]=rand() %100;
+	}
+	
+    for(i4=0; i4<5; i4++) {
+    	score[i4]=grade2[i4];
+	}
+	
+	for(i4=0; i4<5; i4++){
+    	printf("score[%d]=%d\n", i4,score[i4]);
+	}
+	
+	printf("\n");
 	return 0;
 }
